ad1cdd0febf62132a6998dc288f4746412a6d38f
Hybrionix <rene@hybrionix.com>
2025-06-05 15:34:10 +0200

    Revert "Update lang.js"
    This reverts commit b3cb470e1cf26a3683e494c817ea70f2190951ae.

--- src/utils/lang.js
+++ src/utils/lang.js
@@ -1,8 +1,6 @@
 export function getLangProps(pathname) {
   const lang = pathname.startsWith('/en') ? 'en' : 'nl';
-  // Remove '/en' prefix for nlPath, keep the rest
-  const nlPath = pathname.replace(/^\/en/, '') || '/';
-  // Always remove '/en' for enPath as well, if you want both to point to the same path
-  const enPath = nlPath;
+  const nlPath = lang === 'en' ? pathname.replace(/^\/en/, '') || '/' : pathname;
+  const enPath = lang === 'nl' ? '/en' + (pathname === '/' ? '' : pathname) : pathname;
   return { lang, nlPath, enPath };
 }
