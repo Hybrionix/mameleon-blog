dce3b74588ce01929f72c83078967f9951464994
Hybrionix <rene@hybrionix.com>
2025-06-06 00:37:39 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://www.mameleon.com",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
